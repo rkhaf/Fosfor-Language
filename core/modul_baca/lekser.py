b6909fda6d1275f73cef9991a9db60a7ccc14829
rkhaf <rakhafadhilah2006@gmail.com>
2026-07-25 21:06:13 +0700

    bersih2 lekser & masang dokumentasi singkat

--- core/modul_baca/lekser.py
+++ core/modul_baca/lekser.py
@@ -33,12 +33,8 @@ class lekserClass:
         self.temp : str = ""
         self.currentChar : str = ""
         self.forwardChar : str = ""
-        # dotCount : int = 0
         self.fileOriginal : str = ""
         self.tokens : list[Token] = []
-        # invalidFlag : bool = False
-        # self.commentFlag : bool = False
-    
     def maju(self) -> None:
         self.pointerIterator+=1
         self.kolomIterator+=1
@@ -87,15 +83,19 @@ class lekserClass:
         dotCount : int = 0
         
         while self.pointerIterator < len(p_fileMentahan):
-            self.fileOriginal = p_fileMentahan
+            self.fileOriginal = p_fileMentahan #nyalin sourcecode
+            
+            #ngesinkronin currentChar
             self.currentChar = self.fileOriginal[self.pointerIterator]
             if(self.pointerIterator<len(self.fileOriginal)-1):
                 self.forwardChar = self.fileOriginal[self.pointerIterator+1]
             
+            # state default
             if(self.state==states.default):
                 dotCount=0
                 invalidFlag=False
                 
+                # if dibawah ini smpe ke else fungsinya bwt deteksi state doang
                 if(self.currentChar+self.forwardChar==tataBahasa.CMNT_SNGL):
                     self.gantiState(states.singleLineComment)
                     
@@ -112,14 +112,9 @@ class lekserClass:
                     
                 elif(self.currentChar in simbolList.keys() or self.currentChar in perbandinganList.keys() or self.currentChar in operatorList.keys()):
                     self.gantiState(states.simbol)
-                
-                # elif(self.currentChar in perbandinganList.keys()):
-                #     self.gantiState(states.simbol)
-                
+
                 elif(self.currentChar==tataBahasa.KEYWORD_DLMR):
-                    # self.simpenCharKeTemp()
                     self.konversiTempJikaBerisi()
-                    # self.clearTemp()
                     self.simpenCharKeTemp()
                     self.konversiDanPushKeToken()
                     self.maju()
@@ -136,74 +131,72 @@ class lekserClass:
                 
                 else:
                     self.gantiState(states.identifier)
-                        
-                # elif(self.currentChar=="\n"):
-                #     self.gantiBaris()
-                #     self.maju()
-                # else:
-                #     self.maju()
-                
+
+            # state numerik
             elif(self.state==states.numerik):
+                # deteksi apkh karakter numerik
                 if(self.currentChar.isdigit()):
                     self.simpenCharKeTemp()
                     self.maju()
 
+                # deteksi dot buat float
                 elif(self.currentChar=="."):
+                    # ngecek validitas pengisian literal float
                     if(dotCount<1):
                         dotCount+=1
                         self.simpenCharKeTemp()
                         self.maju()
-                    else:   
-                        # self.errorhandlerObjek.tambahinError(self.barisIterator, self.kolomIterator, __name__, self.temp, 1)
+                    else:
                         self.errorhandlerObjek.tambahinError(__name__, 1, self.barisIterator, self.kolomIterator, self.temp)
-                        # self.errorhandlerObjek.kirimError(__name__, 1, self.barisIterator, self.kolomIterator, self.temp)
                         self.maju()
-                        # return self.errorhandlerObjek.kirimError(self.barisIterator, self.kolomIterator, __name__, self.temp, 1)
-                # elif(self.currentChar in kurungList or self.currentChar in punctuationList):
-                #     self.gantiState(states.punctuatorBracket)
                     
                 else:
+                    # selain elemen yg boleh dibaca berbarengan dgn numerik
                     if(self.currentChar==" " or self.currentChar=="\n" or self.currentChar==tataBahasa.KEYWORD_DLMR or self.currentChar in kurungList or self.currentChar in punctuationList):
+                        # ngecek apkh udh diflag invalid apa nggk
                         if(invalidFlag):
                             # print("invld")
                             self.konversiDanPushKeToken(tataBahasa.T_IVTF)
+                            
                         else:
+                            # ngecek jenis literal
                             if(dotCount<1):
                                 self.konversiDanPushKeToken(tataBahasa.T_LITERAL_INT)
-                                # self.pushTempKeToken(tataBahasa.T_LITERAL_INT)
                                 
                             else:
                                 self.konversiDanPushKeToken(tataBahasa.T_LITERAL_FLOAT)
                         self.gantiState(states.default)
 
                     else:
+                        # ngeflag token jdi invalid
                         invalidFlag=True
                         self.simpenCharKeTemp()
                         self.maju()
                         
-            
+            # state string
             elif(self.state==states.string):
                 self.simpenCharKeTemp()
                 self.maju()
+                
+                # ngecek stopper utk stringnya
                 if(p_fileMentahan[self.pointerIterator]=='"'):
                     self.konversiDanPushKeToken(tataBahasa.T_LITERAL_STR)
                     self.state=states.default
                     self.maju()
-                if(self.pointerIterator>=7):
-                    pass
             
+            # state simbol
             elif(self.state==states.simbol):
+                # ngecek apkh char skrg masih didalem simbol, operator, perbandingan
                 if(self.currentChar in simbolList.keys() or self.currentChar in operatorList.keys() or self.currentChar in perbandinganList.keys()):
                     tempMerged : str = self.currentChar+self.forwardChar
-                    if(tempMerged==tataBahasa.CMNT_SNGL):
-                        self.maju()
-                        self.gantiState(states.singleLineComment)
-                    elif(tempMerged in perbandinganList.keys() or tempMerged in operatorList.keys()):
+                    # # state default
+                    # if(tempMerged==tataBahasa.CMNT_SNGL):
+                    #     self.maju()
+                    #     self.gantiState(states.singleLineComment)
+                    if(tempMerged in perbandinganList.keys() or tempMerged in operatorList.keys()):
                         self.konversiTempJikaBerisi()
-                        # self.simpenCharKeTemp()
                         self.simpenKeTemp(tempMerged)
                         self.maju()
-                        # self.simpenCharKeTemp()
                         self.konversiDanPushKeToken()
                         self.maju()
                     else:
@@ -215,7 +208,6 @@ class lekserClass:
                     self.state=states.default
                     
             elif(self.state==states.identifier):
-                # if(self.currentChar==" " or self.currentChar=="\n" or self.currentChar==tataBahasa.KEYWORD_DLMR):
                 if(self.currentChar==" " or self.currentChar=="\n" or self.currentChar==tataBahasa.KEYWORD_DLMR or self.currentChar in perbandinganList.keys() or self.currentChar in operatorList.keys() or self.currentChar+self.forwardChar in perbandinganList.keys()):
                     if(invalidFlag):
                         self.konversiDanPushKeToken(tataBahasa.T_IVTF)
@@ -223,29 +215,11 @@ class lekserClass:
 
                 elif(self.currentChar in kurungList.keys() or self.currentChar in punctuationList.keys()):
                     self.gantiState(states.punctuatorBracket)
-                
-                # elif(self.currentChar==tataBahasa.OPERATOR_PLUS or self.currentChar==tataBahasa.OPERATOR_MINS):
-                #     # self.konversiTempJikaBerisi()
-                #     # self.simpenCharKeTemp()
-                #     # self.konversiDanPushKeToken()
-                #     # self.maju()
-                #     self.gantiState(states.simbol)
-                    
                 elif(self.currentChar in simbolList.keys()):
                     invalidFlag=True
                     self.simpenCharKeTemp()
                     self.maju()
                 else:
-                    # if(self.currentChar==tataBahasa.OPERATOR_DIVE and self.forwardChar==tataBahasa.OPERATOR_DIVE):
-                    #     self.maju()
-                    #     self.gantiState(states.singleLineComment)
-                        
-                    # elif(self.currentChar==tataBahasa.OPERATOR_DIVE and self.forwardChar==tataBahasa.OPERATOR_MULT):
-                    #     self.maju()
-                    #     self.gantiState(states.multiLineComment)
-                        
-                    # else:
-                    #     self.simpenCharKeTemp()
                     self.simpenCharKeTemp()
                     self.maju()
             
@@ -271,7 +245,7 @@ class lekserClass:
                 elif(self.currentChar==tataBahasa.OPERATOR_DIVE and self.forwardChar==tataBahasa.OPERATOR_MULT):
                     kedalamanMultiComment+=1
                     self.maju()
-                    # self.gantiState(states.multiLineComment)
+                    
                 else:
                     self.maju()
                 
@@ -285,12 +259,3 @@ class lekserClass:
                     self.gantiState(states.default)
         else:
             self.konversiTempJikaBerisi()
-            # if(len(self.temp)>0):
-            #     self.konversiDanPushKeToken()
-            # self.konversiDanPushKeToken()
-            
-        # print("\n")
-        # for token in self.tokens:
-        #     print("[",token.tipe,":", token.nilai,"]")
-        #     if("T_DLMR" == token.tipe):
-        #         print("\n")
